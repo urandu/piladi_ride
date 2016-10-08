facefcf057c070574fd9a8f4773cbc897b8acebb
urandu <bildadnamawa@gmail.com>
2016-10-08 15:56:01 +0300

    urandubot commited changes at Sat Oct  8 15:56:01 EAT 2016 on piladi_ride project

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -10,8 +10,20 @@ class User_model extends CI_Model
 {
 
 
-    function new_user()
+    function new_user($user_name,$user_password,$user_email,$user_phone,$user_status=1)
     {
+
+        $data=array(
+            "user_name"=>$user_name,
+            "user_password"=>$user_password,
+            "user_email"=>$user_email,
+            "user_phone"=>$user_phone,
+            "user_status"=>$user_status
+
+        );
+
+        $this->db->insert("user",$data);
+        return $this->db->insert_id();
 
     }
 
