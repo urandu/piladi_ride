949a4bd41f1294ece677086ae0e3ad031d3c2353
urandu <bildadnamawa@gmail.com>
2016-10-08 18:42:01 +0300

    urandubot commited changes at Sat Oct  8 18:42:01 EAT 2016 on piladi_ride project

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -261,41 +261,41 @@ footer p {
 
 <div class="container">
 
-<div class="page-header" id="banner">
-    <div class="row">
-        <div class="col-sm-6">
-            <h1>Material Design</h1>
-
-            <p class="lead">Based on Google Material design</p>
-        </div>
-        <div class="col-sm-6">
-            <div class="well infobox">
-                <p><em>Material Design for Bootstrap</em> is a theme for Bootstrap 3 which lets you use the new
-                    <a href="http://www.google.com/design/spec/material-design/">Google Material Design</a> in your favorite front-end framework.
-                </p>
-
-                <p>If you like this project you can support me by donating something on Gittip, starring this repository, or
-                    <a href="https://github.com/FezVrasta/bootstrap-material-design/issues">reporting bugs and ideas</a>.</p>
-
-                <p>Read more about <em>Material Design for Bootstrap</em>
-                    <a href="https://github.com/FezVrasta/bootstrap-material-design">at the Github page</a>.</p>
-
-                <a href="https://github.com/FezVrasta/bootstrap-material-design/archive/master.zip" class="btn btn-sup btn-material-pink btn-raised">
-                    <i class="material-icons">file_download</i>
-                    <span>DOWNLOAD</span>
-                </a>
-                <a href="https://www.gittip.com/FezVrasta/" class="btn btn-sup btn-material-brown">
-                    <i class="material-icons">favorite_border</i>
-                    <span>GITTIP</span>
-                </a>
-                <a href="https://github.com/FezVrasta/bootstrap-material-design/issues" class="btn btn-sup btn-material-deeppurple">
-                    <i class="material-icons">info_outline</i>
-                    <span>ISSUES</span>
-                </a>
-            </div>
-        </div>
-    </div>
-</div>
+<!--<div class="page-header" id="banner">-->
+<!--    <div class="row">-->
+<!--        <div class="col-sm-6">-->
+<!--            <h1>Material Design</h1>-->
+<!---->
+<!--            <p class="lead">Based on Google Material design</p>-->
+<!--        </div>-->
+<!--        <div class="col-sm-6">-->
+<!--            <div class="well infobox">-->
+<!--                <p><em>Material Design for Bootstrap</em> is a theme for Bootstrap 3 which lets you use the new-->
+<!--                    <a href="http://www.google.com/design/spec/material-design/">Google Material Design</a> in your favorite front-end framework.-->
+<!--                </p>-->
+<!---->
+<!--                <p>If you like this project you can support me by donating something on Gittip, starring this repository, or-->
+<!--                    <a href="https://github.com/FezVrasta/bootstrap-material-design/issues">reporting bugs and ideas</a>.</p>-->
+<!---->
+<!--                <p>Read more about <em>Material Design for Bootstrap</em>-->
+<!--                    <a href="https://github.com/FezVrasta/bootstrap-material-design">at the Github page</a>.</p>-->
+<!---->
+<!--                <a href="https://github.com/FezVrasta/bootstrap-material-design/archive/master.zip" class="btn btn-sup btn-material-pink btn-raised">-->
+<!--                    <i class="material-icons">file_download</i>-->
+<!--                    <span>DOWNLOAD</span>-->
+<!--                </a>-->
+<!--                <a href="https://www.gittip.com/FezVrasta/" class="btn btn-sup btn-material-brown">-->
+<!--                    <i class="material-icons">favorite_border</i>-->
+<!--                    <span>GITTIP</span>-->
+<!--                </a>-->
+<!--                <a href="https://github.com/FezVrasta/bootstrap-material-design/issues" class="btn btn-sup btn-material-deeppurple">-->
+<!--                    <i class="material-icons">info_outline</i>-->
+<!--                    <span>ISSUES</span>-->
+<!--                </a>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
 
 <!-- Navbar
 ================================================== -->
