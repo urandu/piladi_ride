6defce37735ac0d5c554c30a08fe97599b7b4b10
urandu <bildadnamawa@gmail.com>
2016-10-08 14:08:01 +0300

    urandubot commited changes at Sat Oct  8 14:08:01 EAT 2016 on piladi_ride project

--- application/models/Trip_model.php
+++ application/models/Trip_model.php
@@ -9,6 +9,19 @@
 class Trip_model extends CI_Model{
 
 
-    
+    public function new_trip($vehicle_id,$user_id,$status=1)
+    {
+        /*
+         * status==1 if trip is in progress,
+         * status==2 if trip has ended
+        */
+        $data=array(
+            "vehicle_id"=>$vehicle_id,
+            "user_id"=>$user_id,
+            "status"=>$status
+        );
+
+    }
+    public function update_trip=
 
 }
