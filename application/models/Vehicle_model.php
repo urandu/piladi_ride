55a767464e796a035438a7f57d2a471464c468df
urandu <bildadnamawa@gmail.com>
2016-10-08 14:46:01 +0300

    urandubot commited changes at Sat Oct  8 14:46:01 EAT 2016 on piladi_ride project

--- application/models/Vehicle_model.php
+++ application/models/Vehicle_model.php
@@ -12,6 +12,16 @@ class Vehicle_model extends CI_Model{
     public function new_vehicle($vehicle_type,$vehicle_alias,$vehicle_registration,$vehicle_description)
     {
 
+        $data=array(
+            "vehicle_type"=>$vehicle_type,
+            "vehicle_alias"=>$vehicle_alias,
+            "vehicle_registration"=>$vehicle_registration,
+            "vehicle_description"=>$vehicle_description
+        );
+
+        $this->db->insert("vehicle",$data);
+        return $this->db->insert_id();
+
     }
 
 
