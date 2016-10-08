c2b9aa383f6068025208d69755d085d607a51da4
urandu <bildadnamawa@gmail.com>
2016-10-08 15:46:01 +0300

    urandubot commited changes at Sat Oct  8 15:46:01 EAT 2016 on piladi_ride project

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,44 +1,38 @@
 <?php
+
 /**
  * Created by IntelliJ IDEA.
  * User: urandu
  * Date: 10/8/16
  * Time: 2:00 PM
  */
+class User_model extends CI_Model
+{
 
-class Trip_data_model extends CI_Model{
 
-
-    public function new_trip_data($trip_id,$longitude,$latitude)
+    function new_user()
     {
-
-        $data=array(
-            "trip_id"=>$trip_id,
-            "longitude"=>$longitude,
-            "latitude"=>$latitude
-        );
-
-        $this->db->insert("trip_data",$data);
-        return $this->db->insert_id();
 
     }
 
-    public function get_trip_data($trip_id)
+    function update_user()
     {
-        $this->db->where("trip_id",$trip_id);
-        $result=$this->db->get("trip_data");
-
-        if($result->num_rows()>0)
-        {
-            return $result->result();
-        }
-        else
-        {
-            return false;
-        }
 
     }
 
+    function deactivate_user()
+    {
 
+    }
+
+    function get_user()
+    {
+
+    }
+
+    function get_all_user()
+    {
+
+    }
 
 }
