97fac40bf475a6cb898d1563ee60b101fc4cef60
urandu <bildadnamawa@gmail.com>
2016-10-11 20:34:02 +0300

    urandubot commited changes at Tue Oct 11 20:34:02 EAT 2016 on piladi_ride project

--- application/controllers/Trip.php
+++ application/controllers/Trip.php
@@ -29,4 +29,38 @@ class Trip extends CI_Controller {
 
     }
 
+    public function end_trip()
+    {
+
+        $trip_id=$this->input->post("trip_id");
+
+
+        $this->load->model("trip_model");
+        $this->trip_model->end_trip($trip_id);
+        echo(json_encode(array(
+            "status"=>"ok",
+            "trip_id"=>$trip_id
+        )));
+
+    }
+
+    public function new_trip_data()
+    {
+
+        $trip_id=$this->input->post("trip_id");
+        $longitude=$this->input->post("longitude_id");
+        $latitude=$this->input->post("latitude_id");
+
+
+        $this->load->model("trip_data_model");
+        $trip_data_id=$this->trip_data_model->new_trip_data($trip_id,$longitude,$latitude);
+        echo(json_encode(array(
+            "status"=>"ok",
+            "trip_data_id"=>$trip_data_id
+        )));
+
+    }
+
+    
+
 }
