710b833d150b2ed765d65ca78f63e78b28761667
urandu <bildadnamawa@gmail.com>
2016-10-11 21:04:01 +0300

    urandubot commited changes at Tue Oct 11 21:04:01 EAT 2016 on piladi_ride project

--- application/controllers/Vehicle.php
+++ application/controllers/Vehicle.php
@@ -7,7 +7,7 @@ class Vehicle extends CI_Controller {
 	public function index()
 	{
 		//$this->load->view('welcome_message');
-        echo("Welcome to the trip controller");
+        echo("Welcome to the trip vehicle");
 	}
 
     public function new_vehicle()
@@ -16,11 +16,13 @@ class Vehicle extends CI_Controller {
          * status==1 if trip is in progress,
          * status==2 if trip has ended
         */
-       $vehicle_id=$this->input->post("vehicle_id");
-       $user_id=$this->input->post("user_id");
+       $vehicle_type=$this->input->post("vehicle_type");
+       $vehicle_alias=$this->input->post("vehicle_alias");
+       $vehicle_registration=$this->input->post("vehicle_registration");
+       $vehicle_description=$this->input->post("vehicle_description");
 
-        $this->load->model("trip_model");
-        $trip_id=$this->trip_model->new_trip($vehicle_id,$user_id);
+        $this->load->model("vehicle_model");
+        $trip_id=$this->vehicle_model->ew_vehicle($vehicle_type,$vehicle_alias,$vehicle_registration,$vehicle_description);
         echo(json_encode(array(
             "status"=>"ok",
             "trip_id"=>$trip_id
