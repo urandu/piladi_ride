39907d127822e7b732c91bad4352510ad52fd2d2
urandu <bildadnamawa@gmail.com>
2016-10-11 21:12:29 +0300

    work on controllers

--- application/controllers/Vehicle.php
+++ application/controllers/Vehicle.php
@@ -12,77 +12,44 @@ class Vehicle extends CI_Controller {
 
     public function new_vehicle()
     {
-        /*
-         * status==1 if trip is in progress,
-         * status==2 if trip has ended
-        */
+
        $vehicle_type=$this->input->post("vehicle_type");
        $vehicle_alias=$this->input->post("vehicle_alias");
        $vehicle_registration=$this->input->post("vehicle_registration");
        $vehicle_description=$this->input->post("vehicle_description");
 
         $this->load->model("vehicle_model");
-        $trip_id=$this->vehicle_model->ew_vehicle($vehicle_type,$vehicle_alias,$vehicle_registration,$vehicle_description);
+        $vehicle_id=$this->vehicle_model->new_vehicle($vehicle_type,$vehicle_alias,$vehicle_registration,$vehicle_description);
         echo(json_encode(array(
             "status"=>"ok",
-            "trip_id"=>$trip_id
+            "vehicle_id"=>$vehicle_id
         )));
 
 
     }
 
-    public function end_trip()
+
+
+
+
+    public function get_vehicle()
     {
+        $vehicle_id=$this->input->post("vehicle_id");
+        $this->load->model("vehicle_model");
+        $vehicle=$this->vehicle_model->get_vehicle($vehicle_id);
 
-        $trip_id=$this->input->post("trip_id");
-
-
-        $this->load->model("trip_model");
-        $this->trip_model->end_trip($trip_id);
-        echo(json_encode(array(
-            "status"=>"ok",
-            "trip_id"=>$trip_id
-        )));
-
-    }
-
-    public function new_trip_data()
-    {
-
-        $trip_id=$this->input->post("trip_id");
-        $longitude=$this->input->post("longitude_id");
-        $latitude=$this->input->post("latitude_id");
-
-
-        $this->load->model("trip_data_model");
-        $trip_data_id=$this->trip_data_model->new_trip_data($trip_id,$longitude,$latitude);
-        echo(json_encode(array(
-            "status"=>"ok",
-            "trip_data_id"=>$trip_data_id
-        )));
-
-    }
-
-
-
-    public function get_trip_data()
-    {
-        $trip_id=$this->input->post("trip_id");
-        $this->load->model("trip_data_model");
-        $trip_data=$this->trip_data_model->new_trip_data($trip_id);
-
-        if($trip_data!==false)
+        if($vehicle!==false)
         {
             echo(json_encode(array(
                 "status"=>"ok",
-                "trip_data"=>$trip_data
+                "vehicle"=>$vehicle
             )));
         }
         else
         {
             echo(json_encode(array(
                 "status"=>"error",
-                "trip_data"=>"no data"
+                "vehicle"=>"vehicle not found"
             )));
         }
 
